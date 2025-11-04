85aed02911314e4ecb38575dc809261e52cd6f9d
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-11-03 19:54:00 -0500

    Auto refreshToken

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,12 +1,26 @@
 import SignOut from '@/components/auth/signout';
+import { Button } from '@/components/ui/button';
+import api from '@/lib/axios';
 import { useAuthStore } from '@/stores/useAuthStore';
+import { toast } from 'sonner';
 
 const ChatAppPage = () => {
   const { user } = useAuthStore();
+  const handleOnClick = async () => {
+    try {
+      await api.get('/users/test', { withCredentials: true });
+      toast.success('Test API call successful');
+    } catch (error) {
+      console.error(error);
+      toast.error('Test API call failed');
+      console.error(error);
+    }
+  };
   return (
     <div>
       {user?.username}
       <SignOut />
+      <Button onClick={handleOnClick}>Test</Button>
     </div>
   );
 };
