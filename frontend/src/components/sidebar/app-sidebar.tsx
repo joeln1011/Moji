e41b8e07a199c162b6c9b05bead6872e2a0d7d4a
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2026-04-30 21:37:05 -0400

    Dark Mode

---
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { Moon, Sun } from 'lucide-react';
+import { Switch } from '../ui/switch';
+import CreateNewChat from '../chat/CreateNewChat';
+import NewGroupChatModal from '../chat/NewGroupChatModal';
+import GroupChatList from '../chat/GroupChatList';
+import AddFriendModal from '../chat/AddFriendModal';
+import DirectMessageList from '../chat/DirectMessageList';
+import { useThemeStore } from '@/stores/useThemeStore';
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { isDark, toggleTheme } = useThemeStore();
+
+  return (
+    <Sidebar variant="inset" {...props}>
+      {/* Header */}
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              size="lg"
+              asChild
+              className="bg-gradient-primary"
+            >
+              <a href="#">
+                <div className="flex w-full items-center px-2 justify-between">
+                  <h1 className="text-xl font-bold text-white">Moji</h1>
+                  <div className="flex items-center gap-2">
+                    <Sun className="size-4 text-white/80" />
+                    <Switch
+                      checked={isDark}
+                      onCheckedChange={toggleTheme}
+                      className="data-[state=checked]:bg-background/80"
+                    />
+                    <Moon className="size-4 text-white/80" />
+                  </div>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      {/* Content */}
+      <SidebarContent>
+        {/* New chat */}
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <CreateNewChat />
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Group Chat */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="uppercase">
+            Group Chats
+          </SidebarGroupLabel>
+          <SidebarGroupAction title="Create Group" className="cursor-pointer">
+            <NewGroupChatModal />
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            <GroupChatList />
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Direct Messages */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="uppercase">Friends</SidebarGroupLabel>
+
+          <SidebarGroupAction title="Add Friend" className="cursor-pointer">
+            <AddFriendModal />
+          </SidebarGroupAction>
+
+          <SidebarGroupContent>
+            <DirectMessageList />
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      {/* Footer */}
+      <SidebarFooter>{/* <NavUser user={data.user} /> */}</SidebarFooter>
+    </Sidebar>
+  );
+}
