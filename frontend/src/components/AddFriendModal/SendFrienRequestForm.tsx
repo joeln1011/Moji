ef698328bd6b7e9b80db2a0f6becaa4539daf498
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2026-05-08 22:47:17 -0400

    Add Friend Modal

---
+++ frontend/src/components/AddFriendModal/SendFrienRequestForm.tsx
@@ -0,0 +1,72 @@
+import type { UseFormRegister } from 'react-hook-form';
+import type { IFormValues } from '../chat/AddFriendModal';
+import { Label } from '../ui/label';
+import { Textarea } from '../ui/textarea';
+import { DialogFooter } from '../ui/dialog';
+import { Button } from '../ui/button';
+import { UserPlus } from 'lucide-react';
+
+interface SendRequestProps {
+  register: UseFormRegister<IFormValues>;
+  loading: boolean;
+  searchedUsername: string;
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+  onBack: () => void;
+}
+const SendFrienRequestForm = ({
+  register,
+  loading,
+  searchedUsername,
+  onSubmit,
+  onBack,
+}: SendRequestProps) => {
+  return (
+    <form onSubmit={onSubmit} className="space-y-4">
+      <div>
+        <span className="success-message">
+          Found <span className="font-semibold">@{searchedUsername} !!!</span>
+        </span>
+
+        <div className="space-y-2">
+          <Label htmlFor="message" className="text-sm font-semibold">
+            Introduce
+          </Label>
+          <Textarea
+            id="message"
+            rows={3}
+            placeholder="Write a message..."
+            className="glass border-boder/50 focus:border-primary/50 transition-smooth resize-none"
+            {...register('message')}
+          />
+        </div>
+
+        <DialogFooter>
+          <Button
+            type="button"
+            variant="outline"
+            className="flex-1 glass hover:text-destructive"
+            onClick={onBack}
+          >
+            Back
+          </Button>
+
+          <Button
+            type="submit"
+            disabled={loading}
+            className="flex-1 bg-gradient-chat text-white hover:opacity-90 transition-smooth"
+          >
+            {loading ? (
+              <span>Sending...</span>
+            ) : (
+              <>
+                <UserPlus className="size-4 mr-2" /> Add Friend
+              </>
+            )}
+          </Button>
+        </DialogFooter>
+      </div>
+    </form>
+  );
+};
+
+export default SendFrienRequestForm;
