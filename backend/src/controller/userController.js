f7d98236510f32f5a6b591c9c1d9e93c8782b10f
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2026-05-11 17:36:43 -0400

    User Avatar

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,3 +1,4 @@
+import { uploadImageFromBuffer } from '../middlewares/uploadMiddleware.js';
 import User from '../models/User.js';
 
 export const authMe = async (req, res) => {
@@ -23,5 +24,38 @@ export const searchUserByUsername = async (req, res) => {
   } catch (error) {
     console.error('Search user error:', error);
     return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const uploadAvatar = async (req, res) => {
+  try {
+    const file = req.file;
+    const userId = req.user._id;
+
+    if (!file) {
+      return res.status(400).json({ message: 'No file uploaded' });
+    }
+
+    const result = await uploadImageFromBuffer(file.buffer);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        avatarUrl: result.secure_url,
+        avatarId: result.public_id,
+      },
+      {
+        new: true,
+      },
+    ).select('avatarUrl');
+
+    if (!updatedUser.avatarUrl) {
+      return res.status(400).json({ message: 'Avatar trả về null' });
+    }
+
+    return res.status(200).json({ avatarUrl: updatedUser.avatarUrl });
+  } catch (error) {
+    console.error('Lỗi xảy ra khi upload avatar', error);
+    return res.status(500).json({ message: 'Upload failed' });
   }
 };
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { authMe, searchUserByUsername } from '../controller/userController.js';
-
+import {
+  authMe,
+  searchUserByUsername,
+  uploadAvatar,
+} from '../controller/userController.js';
+import { upload } from '../middlewares/uploadMiddleware.js';
 const router = express.Router();
 
 router.get('/me', authMe);
 router.get('/search', searchUserByUsername);
-
+router.post('/uploadAvatar', upload.single('file'), uploadAvatar);
 export default router;
